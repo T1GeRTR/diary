867d088148530f113fce720288d6fdc5c2ab4cb4
Талгат Ракишев <>
2020-12-03 21:00:36 +0600

    all service tests

--- sql/diary.sql
+++ sql/diary.sql
@@ -9,7 +9,7 @@ CREATE TABLE `project` (
     `name` varchar(50) NOT NULL,
     `preview` varchar(50) NOT NULL,
     PRIMARY KEY (id),
-    KEY name (name),
+    UNIQUE KEY name (name),
     KEY preview (preview)
 ) ENGINE=INNODB;
 
@@ -22,7 +22,7 @@ CREATE TABLE `skill` (
     KEY icon (icon)
 ) ENGINE=INNODB;
 
-CREATE TABLE `linkType` (
+CREATE TABLE `link_type` (
     `id` int NOT NULL AUTO_INCREMENT,
     `name` varchar(50) NOT NULL,
     `icon` varchar(50) NOT NULL,
@@ -33,18 +33,24 @@ CREATE TABLE `linkType` (
 
 CREATE TABLE `project_skill` (
     `id` int NOT NULL AUTO_INCREMENT,
-    `projectId` int NOT NULL,
-    `skillId` int NOT NULL,
+    `project_id` int NOT NULL,
+    `skill_id` int NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (projectId) REFERENCES `project` (id) ON DELETE CASCADE,
-    FOREIGN KEY (skillId) REFERENCES `skill` (id) ON DELETE CASCADE
+    FOREIGN KEY (project_id) REFERENCES `project` (id) ON DELETE CASCADE,
+    FOREIGN KEY (skill_id) REFERENCES `skill` (id) ON DELETE CASCADE
 ) ENGINE=INNODB;
 
-CREATE TABLE `project_link` (
+CREATE TABLE `link` (
     `id` int NOT NULL AUTO_INCREMENT,
-    `projectId` int NOT NULL,
-    `linkId` int NOT NULL,
+    `link_type_id` int NOT NULL,
+    `project_id` int NOT NULL,
+    `url` varchar(50) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (projectId) REFERENCES `project` (id) ON DELETE CASCADE,
-    FOREIGN KEY (linkId) REFERENCES `linkType` (id) ON DELETE CASCADE
+    FOREIGN KEY (link_type_id) REFERENCES `link_type` (id) ON DELETE CASCADE,
+    FOREIGN KEY (project_id) REFERENCES `project` (id) ON DELETE CASCADE,
+    UNIQUE KEY url (url)
 ) ENGINE=INNODB;
+
+Insert into project values (1, "project1", "preview");
+Insert into link_type values (1, "github", "icon");
+Insert into skill values(1, "Java", "icon");
